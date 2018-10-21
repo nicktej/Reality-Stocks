179fe169e357e639a49a87c39685efb715e3ce50
Chase King <chase@chaseking.me>
2018-10-21 00:36:52 -0700

    Done for the night 😴 Ready for graphs and stuff =D

--- node-backend/client/src/components/MainSlider.js
+++ node-backend/client/src/components/MainSlider.js
@@ -1,71 +1,140 @@
 // Inspired by Corey Haggards "Screeners"
 // https://dribbble.com/shots/4138489-Screeners
 
-import React, { Fragment } from "react";
-import { Parallax, ParallaxLayer } from "react-spring";
+import React, { Component, Fragment } from "react";
+import { Spring, Parallax, ParallaxLayer } from "react-spring";
+import moment from "moment";
 
-const MainPage = ({ offset, caption, first, second, gradient, onClick }) => (
-    <Fragment>
-        <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
-            <div className="slopeBegin" />
-        </ParallaxLayer>
+const COMPANIES = [
+    {
+        company: "Alphabet Inc",
+        ticker: "GOOGL",
+        gradient: ["#FF1493", "#FF7F50"]
+    },
+    {
+        company: "Starbucks Corporation",
+        ticker: "SBUX",
+        gradient: ["#6A5ACD", "#00BFFF"]
+    }
+];
 
-        <ParallaxLayer offset={offset} speed={-0.2} onClick={onClick}>
-            <div className={`slopeEnd ${gradient}`} />
-        </ParallaxLayer>
+class Clock extends Component {
+    componentWillMount() {
+        this.taskId = setInterval(() => {
+            let now = moment();
 
-        <ParallaxLayer className="text number" offset={offset} speed={0.3}>
-            <span>0{offset + 1}</span>
-        </ParallaxLayer>
+            this.setState({
+                time: now.format("h:mm:ss"),
+                amPm: now.format("A")
+            });
+        }, 1000);
+    }
 
-        <ParallaxLayer className="text header" offset={offset} speed={0.4}>
-            <span>
-                <p style={{ fontSize: 20 }}>{caption}</p>
-                <div className={`stripe ${gradient}`} />
-                <p>{first}</p>
-                <p>{second}</p>
-            </span>
-        </ParallaxLayer>
-    </Fragment>
-)
+    componentDidUnmount() {
+        clearInterval(this.taskId);
+    }
 
-export default class MainSlider extends React.Component {
+    render() {
+        if (!this.state) return null;
+
+        return (
+            <div className="clock clock-green">{this.state.time} <small>{this.state.amPm}</small></div>
+        )
+    }
+}
+
+class Company extends Component {
+    render() {
+        return (
+            <ParallaxLayer className={"company-container" + (this.props.expanded ? " expanded" : "")} offset={this.props.offset} speed={0} onClick={this.props.onClick}>
+                <h1 className="company-name">{this.props.company}</h1>
+                <h2 className="company-ticker">{this.props.ticker}</h2>
+
+                <div className="stock-info-slides">
+                    SAMPLE STUFF HERE
+                    <Clock />
+                </div>
+            </ParallaxLayer>
+        )
+    }
+}
+
+export default class MainSlider extends Component {
     state = {
-        currentPage: 0
+        index: 0,
+        expanded: false
     };
 
     setPage = index => {
         if (index < 0) {
-            index = 2;
-        } else if (index > 2) {
+            index = COMPANIES.length - 1;
+        } else if (index >= COMPANIES.length) {
             index = 0;
         }
 
-        this.setState({ currentPage: index });
+        this.setState({ index });
         this.parallax.scrollTo(index);
     }
 
     componentDidMount() {
         document.addEventListener("keydown", event => {
-            console.log(event);
-
             if (event.keyCode === 37) {
-                this.setPage(this.state.currentPage - 1);
+                // Left arrow
+                if (this.state.expanded) return;
+                this.setPage(this.state.index - 1);
+            } else if (event.keyCode === 38) {
+                // Up arrow
+                this.setState({ expanded: false });
             } else if (event.keyCode === 39) {
-                this.setPage(this.state.currentPage + 1);
+                // Right arrow
+                if (this.state.expanded) return;
+                this.setPage(this.state.index + 1);
             } else if (event.keyCode === 40) {
-
+                // Down arrow
+                this.setState({ expanded: true });
             }
-        })
+        });
     }
 
     render() {
+        let currentCompany = COMPANIES[this.state.index];
+
         return (
-            <Parallax className="container" ref={e => this.parallax = e} pages={3} horizontal scrolling={false}>
-                <Page offset={0} gradient="pink" caption="Page 1" first="Header" second="Subheader" onClick={() => this.setPage(1)} />
-                <Page offset={1} gradient="teal" caption="Page 2" first="Header" second="Subheader" onClick={() => this.setPage(2)} />
-                <Page offset={2} gradient="tomato" caption="Page 2" first="Header" second="Subheader" onClick={() => this.setPage(0)} />
-            </Parallax>
-        )
+            <Spring
+                from={{
+                    color: "black",
+                    gradientMove: 100,
+                }}
+                to={{
+                    gradientStart: currentCompany.gradient[0],
+                    gradientEnd: currentCompany.gradient[1],
+                    gradientMove: 0
+                }}
+                reset
+                config={{ tension: 25, friction: 8 }}
+            >
+                {props => (
+                    <Parallax
+                        className="companies-container"
+                        style={{
+                            background: this.state.expanded ? ("linear-gradient(to bottom, " + props.gradientEnd + " 0%, white " + props.gradientMove + "%)") : ("linear-gradient(to right, " + props.gradientStart + " 0%, " + props.gradientEnd + " 100%)"),
+                        }}
+                        ref={e => this.parallax = e}
+                        pages={COMPANIES.length}
+                        horizontal scrolling={false}
+                        config={{ tension: 30, friction: 15 }}
+                    >
+                        {COMPANIES.map((company, index) => (
+                            <Company
+                                key={company.ticker}
+                                offset={index}
+                                {...company}
+                                expanded={this.state.expanded && this.state.index === index}
+                            />
+                        ))}
+                    </Parallax>
+                )}
+            </Spring>
+        );
     }
 }
